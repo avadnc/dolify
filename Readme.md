9261a0f30d1cefb168cc8724ec373dc623d7bdf8
avadnc <informaticalafell@gmail.com>
2022-08-25 21:52:39 -0600

    commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Nubecommerce
+
+Modify env.example to .env and add DOLIBARR_APP="" with absolute path to main.inc.php 
--- application/controllers/ProductController.php
+++ application/controllers/ProductController.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once DOLIBARR_APP . 'master.inc.php';
+require_once getenv('DOLIBARR_APP') . 'master.inc.php';
 
 class ProductController extends CI_Controller
 {
